96e3b705b5ffb66698e8dca609560616773ad42b
silin098 <dearjohn544@gmail.com>
2023-08-31 00:39:21 +0800

    fix something

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'home'=>'Home',
+    'about'=>'about',
+    'contact'=>'contact',
+    'careers'=>'careers',
+    'news'=>'news',
+];
--- resources/views/crypto.blade.php
+++ resources/views/crypto.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 @section('content')
 
-
+    <div class="container">
     <div class="row justify-content-center">
         @foreach($jsonData->hits as $blog)
 
-            <div class="col-md-5 col-12 border mt-3 me-3 py-1 card-hover rounded news-font">
+            <div class="col-md-5 col-sm-12  border mt-3 ms-2 py-1 card-hover rounded news-font">
                 <a class="text-decoration-none" href="{{$blog->url}}" target="_blank">
-{{--                <p class="text-secondary fw-bold">Updated: Sep 27, 2022 </p>--}}
+
                 <h5 class="text-white fw-bold mt-2">{{$blog->title}}</h5>
                 <p class="text-secondary fw-bold text-capitalize">Author : {{$blog->author}}</p>
                 </a>
             </div>
         @endforeach
-
+    </div>
 
     </div>
 
