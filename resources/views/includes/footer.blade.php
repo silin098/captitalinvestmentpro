50f5beb45b3ec6a7d73c2316dd90fefaf98d5fbf
silin098 <dearjohn544@gmail.com>
2023-11-03 23:50:03 +0800

    redesign the ui

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,25 +1,55 @@
 
-    <footer class="container">
-    <div class="d-flex flex-wrap justify-content-between align-items-center my-5">
-    <div class="col-md-4 d-flex align-items-center">
-        <a href="/" class="mb-3 mb-md-0 text-body-secondary text-decoration-none lh-1 pe-2">
-            <img src="{{url('images/logo12.png')}}"   height="30"/>
-        </a>
-        <span class="mb-3 mb-md-0">&copy; {{now()->year}} Capitalinvestmentpro, Inc</span>
-    </div>
+{{--    <footer class="container">--}}
+{{--    <div class="d-flex flex-wrap justify-content-between align-items-center my-5">--}}
+{{--    <div class="col-md-3">--}}
+{{--        <a href="/" class="mb-3 mb-md-0 text-body-secondary text-decoration-none lh-1">--}}
+{{--            <img src="{{url('images/logo12.png')}}"  height="30"/>--}}
+{{--        </a>--}}
+{{--        <span class="mb-3 mb-md-0">&copy; {{now()->year}} Capitalinvestmentpro, Inc</span>--}}
+{{--    </div>--}}
 
-    <ul class="nav col-md-4  justify-content-center list-unstyled d-flex me-md-auto">
-        <li class="ms-3"><a class="text-secondary" href="{{route('about')}}">About</a></li>
-        <li class="ms-3"><a class="text-secondary" href="{{route('contact')}}">Contact</a></li>
-        <li class="ms-3"><a class="text-secondary" href="{{route('career')}}">Careers</a></li>
-        <li class="ms-3"><a class="text-secondary" href="{{route('policy')}}">Policy</a></li>
+{{--    <ul class="nav col-md-4 justify-content-center list-unstyled d-flex me-md-auto">--}}
+{{--        <li class="ms-3"><a class="text-secondary" href="{{route('about')}}">About</a></li>--}}
+{{--        <li class="ms-3"><a class="text-secondary" href="{{route('contact')}}">Contact</a></li>--}}
+{{--        <li class="ms-3"><a class="text-secondary" href="{{route('career')}}">Careers</a></li>--}}
+{{--        <li class="ms-3"><a class="text-secondary" href="{{route('policy')}}">Policy</a></li>--}}
 
-    </ul>
-    <div class="mx-end py-3">
-        <a  class="ms-2" href="https://t.me/JayK454" target="_blank"><i class="fa-brands fa-telegram fa-2xl text-primary"></i></a>
-        <a  class="ms-2" href="https://wa.me/+66612790081" target="_blank"><i class="fa-brands fa-whatsapp fa-2xl text-primary"></i></a>
+{{--    </ul>--}}
+{{--    <div class="py-3 mx-end">--}}
+{{--        <a  class="ms-2" href="https://t.me/JayK454" target="_blank"><i class="fa-brands fa-telegram fa-2xl text-primary"></i></a>--}}
+{{--        <a  class="ms-2" href="https://wa.me/+66612790081" target="_blank"><i class="fa-brands fa-whatsapp fa-2xl text-primary"></i></a>--}}
+{{--    </div>--}}
+{{--</div>--}}
+{{--    </footer>--}}
+
+<footer class="bg-light  py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h4>Capital Investment Pro</h4>
+                <p>We specialize in providing seamless and secure solutions for buying, selling, and managing digital assets.</p>
+            </div>
+            <div class="col-md-3">
+                <h4>Quick Links</h4>
+                <ul class="list-unstyled">
+                    <li><a href="{{route('home')}}" class="nav-link">Home</a></li>
+                    <li><a href="{{route('about')}}" class="nav-link" >About</a></li>
+                    <li><a href="{{route('career')}}" class="nav-link">Career</a></li>
+                    <li><a href="{{route('contact')}}" class="nav-link">Contact</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h4>Contact</h4>
+                <address>
+                    95 Tak Ok, Ban Tak District,<br>
+                    Tak, Thailand<br>
+
+                    <a href="mailto:info@capitalinvestmentpro.com" class="nav-link">info@capitalinvestmentpro.com</a>
+                </address>
+            </div>
+        </div>
     </div>
-</div>
-    </footer>
+</footer>
+
 
 
